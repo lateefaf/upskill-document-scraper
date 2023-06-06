288ef3d84a2f7eebad622a4e72765e4e52b25d57
Afnan <alateef4@gmail.com>
2023-06-05 22:22:38 -0400

    Changes functioon of ddef clone_filesystem_structure,

--- utils/file_system_utils.py
+++ utils/file_system_utils.py
@@ -1,24 +1,17 @@
 import os
-import shutil
 
-def clone_filesystem_structure(source_dir, target_dir):
+def clone_filesystem_structure(source_dir, destination_dir):
     """
     Clone the folder structure from the source_directory to the destination_directory.
-
+    Only the directory structure is created; files are not copied.
+    
     :param source_dir: The source directory whose folder structure will be cloned.
-    :param target_dir: The directory where the cloned folder structure will be placed.
+    :param destination_dir: The directory where the cloned folder structure will be created.
     """
     for root, dirs, files in os.walk(source_dir):
         # Get the relative path of the current directory from the source_directory
         relative_path = os.path.relpath(root, source_dir)
 
         # Create the corresponding directory structure within the destination_directory
-        destination_path = os.path.join(target_dir, relative_path)
+        destination_path = os.path.join(destination_dir, relative_path)
         os.makedirs(destination_path, exist_ok=True)
-
-        # Copy the files from the source directory to the destination directory
-        for file in files:
-            source_file_path = os.path.join(root, file)
-            destination_file_path = os.path.join(destination_path, file)
-
-            shutil.copy(source_file_path, destination_file_path)
