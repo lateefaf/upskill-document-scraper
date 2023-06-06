f63731423912940aa205ad12eba80594b4a16d34
Afnan <alateef4@gmail.com>
2023-06-05 22:16:54 -0400

    Creates def clone_filesystem_structure and implements it in main.

---
+++ main.py
@@ -0,0 +1,6 @@
+from utils.file_system_utils import clone_filesystem_structure
+
+source_dir = '/path/to/source_directory'
+destination_dir = '/path/to/destination_directory'
+
+clone_filesystem_structure(source_dir, destination_dir)
